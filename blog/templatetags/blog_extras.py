cb760667ebd39f5a8e04cb6176bc56ecfb09ec56
Neha Dubey <dubeyn894@gmail.com>
2025-11-14 23:31:31 +0000

    Finish customtemplate tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -3,7 +3,7 @@ from django import template
 from django.utils.html import escape
 from django.utils.safestring import mark_safe
 from django.utils.html import format_html
-
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
@@ -31,3 +31,18 @@ def author_details(author, current_user ):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")        
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}    
+    
